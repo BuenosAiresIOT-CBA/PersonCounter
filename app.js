02aee4049b643c416e48114a6996d7357e3920ce
martin <martin@rga.com>
2015-02-27 17:18:49 -0300

    moviendo archivos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
 // var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -57,3 +58,41 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
+
+var personCounter = 0;
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+app.get('/count', function(req, res){
+    var field = req.param("field");
+    console.log(field);
+    
+    if (field==1){
+    personCounter++;
+    }else{
+    personCounter--;   
+    }
+    
+    io.emit('arduino', personCounter);
+    res.json({});
+
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+    //Envio el mensaje blink_delay con valor 255 cuando el usuario se conecta
+    io.emit('arduino', personCounter);
+});
+
+
